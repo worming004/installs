4d9b6d82d4885a84c2ca68bcf109d52a5b194e2d
worming <worming@hotmail.be>
2023-02-22 19:53:48 +0100

    add kitty as default terminal

--- init.sh
+++ init.sh
@@ -27,6 +27,10 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf install code
 
+# kitty
+sudo dnf install kitty
+echo "export TERMINAL=/usr/bin/kitty" >> ~/.profile
+
 # asdf
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.1
 . "$HOME/.asdf/asdf.sh"
