ff2601de73987df5afc3e3ea5b230397efcc9a70
worming <worming@hotmail.be>
2023-05-22 19:26:25 +0200

    few more things

--- init.sh
+++ init.sh
@@ -1,3 +1,1 @@
-ansible-galaxy install cimon-io.asdf
-# ansible-playbook deps.yml --ask-become-pass
-KERL_BUILD_DOCS=yes ansible-playbook local.yml --ask-become-pass
+ansible-playbook local.yml --ask-become-pass
